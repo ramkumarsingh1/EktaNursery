a2640c129292deac52ffaf252e49db31d627d65d
Ram Kumar <tharabandhuram@gmail.com>
2026-08-08 17:21:48 +0530

    search

--- client/src/components/ui/SearchBar.jsx
+++ client/src/components/ui/SearchBar.jsx
@@ -1,21 +1,8 @@
-import { FiSearch } from "react-icons/fi";
-
-export default function SearchBar({
-    placeholder = "Search...",
-    value,
-    onChange,
-}) {
+export default function SearchBar() {
     return (
-        <div className="relative">
-            <FiSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
-
-            <input
-                type="text"
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                className="w-full rounded-xl border border-gray-300 px-4 py-3 pl-12 outline-none transition-all duration-200 focus:border-green-600 focus:ring-2 focus:ring-green-200"
-            />
-        </div>
+        <input
+            type="text"
+            placeholder="Search..."
+        />
     );
 }
