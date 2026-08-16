1f30f8933d2a8503ec619c58c35b28345e88670f
Ram Kumar <tharabandhuram@gmail.com>
2026-08-16 12:11:50 +0530

    some css fix in cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import Container from "../components/layout/Container";
 import CartItem from "../components/cart/CartItem";
 import CartSummary from "../components/cart/CartSummary";
+
 export default function Cart() {
     const cartItems = useSelector(
         (state) => state.cart.items
@@ -9,25 +10,37 @@ export default function Cart() {
 
     return (
         <Container>
-            <section className="py-10">
 
-                <h1 className="text-4xl font-bold">
+            <section className="py-4 sm:py-8">
+
+                {/* Heading */}
+                <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
                     Shopping Cart
                 </h1>
+
+
                 {cartItems.length === 0 ? (
-                    <div className="mt-10 rounded-xl border border-dashed p-10 text-center">
-                        <h2 className="text-2xl font-bold">
+
+                    /* Empty Cart */
+                    <div className="mt-5 rounded-lg border border-dashed p-6 text-center sm:mt-8 sm:p-10">
+
+                        <h2 className="text-lg font-semibold text-gray-800 sm:text-2xl">
                             Your Cart is Empty
                         </h2>
 
-                        <p className="mt-2 text-gray-500">
+                        <p className="mt-1 text-sm text-gray-500">
                             Add some plants to continue shopping.
                         </p>
-                    </div>
-                ) : (
-                    <div className="mt-8 grid gap-8 lg:grid-cols-[1fr_350px]">
 
-                        <div className="space-y-5">
+                    </div>
+
+                ) : (
+
+                    /* Cart Content */
+                    <div className="mt-4 grid gap-4 lg:grid-cols-[1fr_320px] lg:items-start sm:mt-6 sm:gap-6">
+
+                        {/* Cart Items */}
+                        <div className="space-y-2 sm:space-y-4">
                             {cartItems.map((item) => (
                                 <CartItem
                                     key={item._id}
@@ -36,12 +49,16 @@ export default function Cart() {
                             ))}
                         </div>
 
+
+                        {/* Price Details */}
                         <CartSummary />
 
                     </div>
+
                 )}
 
             </section>
+
         </Container>
     );
 }
