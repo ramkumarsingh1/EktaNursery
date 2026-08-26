2e745e95957fb36f0f534c4b22822b478d3c63c7
Ram Kumar <tharabandhuram@gmail.com>
2026-08-26 13:53:01 +0530

    search fixing

--- client/src/components/ui/SearchBar.jsx
+++
@@ -1,8 +0,0 @@
-export default function SearchBar() {
-    return (
-        <input
-            type="text"
-            placeholder="Search..."
-        />
-    );
-}
--- client/src/constants/navLinks.js
+++ client/src/constants/navLinks.js
@@ -1,7 +1,6 @@
 export const navLinks = [
   { title: "Home", path: "/" },
   { title: "Shop", path: "/shop" },
-  { title: "Plants", path: "/plants" },
   { title: "About", path: "/about" },
   { title: "Contact", path: "/contact" },
 ];
